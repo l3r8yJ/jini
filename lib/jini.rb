bae55436f78fe193422031118ad2cb94cde4456a
Ivan <clicker.heroes.acg@gmail.com>
2022-09-15 14:28:13 +0300

    docs

--- lib/jini.rb
+++ lib/jini.rb
@@ -10,6 +10,7 @@ class Jini
   # When path not valid
   class InvalidPath < StandardError; end
 
+  # @param head String with head of your xpath
   def initialize(head = '')
     @head = head
   end
@@ -20,30 +21,40 @@ class Jini
   end
 
   # Additional node for xpath.
+  # @param node String node
   def add_path(node)
     Jini.new("#{@head}/#{node}")
   end
 
   # Additional attribute for xpath.
+  # @param key String name of attr
+  # @param value String value of attr
   def add_attr(key, value)
     Jini.new("#{@head}[@#{key}=\"#{value}\"]")
   end
 
+  # Adds an @value to xpath
+  # @param value String with value attr
   def all_attr(value)
     Jini.new("#{@head}@#{value}")
   end
 
   # Xpath with all elements.
+  # Addition a *** to xpath
   def all
     Jini.new(add_path('*').to_s)
   end
 
   # Xpath with all named elements.
+  # Addition _//node_ to xpath
+  # @param node String name of node
   def add_all(node)
     Jini.new("#{@head}//#{node}")
   end
 
   # Access by index.
+  # Addition _[index]_ to xpath
+  # @param position int number
   def at(position)
     Jini.new("#{@head}[#{position}]")
   end
